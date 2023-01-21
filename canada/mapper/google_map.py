f57d6a4c262cef129c7cd5c5a83b0ff0a2a9a75e
paul <kimyb132@gmail.com>
2023-01-22 03:15:12 +0900

    feat - update mapper

--- canada/mapper/google_map.py
+++ canada/mapper/google_map.py
@@ -4,7 +4,7 @@ import googlemaps
 import json
 import pandas as pd
 from loguru import logger
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Tuple
 
 def to_json(file_path, enriched_path_suffix, file_name, file_format, data):
     save_path = f"{file_path}/{enriched_path_suffix}/{file_name}.{file_format}"
@@ -19,25 +19,37 @@ def enrich_data(gm_client, data) -> List[Dict[str, Any]]:
         enriched_row = row
         lat, lng = None, None
         geocode_info = None
-        # if row.get('address') != None:
-        #     try:
-        #         geocode_info = gm_client.geocode('Toronto ' + row['address'])
-        #         lat = geocode_info[0]['geometry']['location']['lat']
-        #         lng = geocode_info[0]['geometry']['location']['lng']
-        #     except Exception as e:
-        #         logger.info(str(e))
-        #         logger.info(row['address'])
-        #         logger.info(f"geocode info: {geocode_info}")
+        if row.get('url') != None:
+            lat = float(row['url'].split('!')[5].split('3d')[-1])
+            lng = float(row['url'].split('!')[6].split('4d')[-1])
+        elif row.get('address') != None:
+            try:
+                geocode_info = gm_client.geocode('Toronto ' + row['address'])
+                lat = geocode_info[0]['geometry']['location']['lat']
+                lng = geocode_info[0]['geometry']['location']['lng']
+            except Exception as e:
+                logger.info(str(e))
+                logger.info(row['address'])
+                logger.info(f"geocode info: {geocode_info}")
+        else:
+            lat, lng = None, None
 
-        # enriched_row['lat'] = lat
-        # enriched_row['lng'] = lng
-        # result.append(enriched_row)
+        enriched_row['lat'] = lat
+        enriched_row['lng'] = lng
+        result.append(enriched_row)
 
     return result
 
-def google_map_data_processor(api_key, file_path,raw_path_suffix, enriched_path_suffix, file_format):
+def google_map_mapper(google_variable):
+    api_key=google_variable.api_key
+    file_path=google_variable.file_path
+    raw_path_suffix=google_variable.raw_path_suffix
+    enriched_path_suffix=google_variable.enriched_path_suffix
+    file_format=google_variable.file_format
+
     gm_client=googlemaps.Client(key=api_key)
     logger.info("Start Processing")
+
     for json_file_path in glob.glob(f"{file_path}/{raw_path_suffix}/*.{file_format}"):
         file_name = json_file_path.split(raw_path_suffix+'/')[-1].split(f'.{file_format}')[0]
         logger.info(f"ETL Process: {file_name}")
